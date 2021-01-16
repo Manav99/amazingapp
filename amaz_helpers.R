6a1b2a57f496af8f1b90f936dc84fa31e3fb681d
Manvendra <manavshrimal99@gmail.com>
2021-01-17 00:11:03 +0530

    Add files via upload

---
+++ amaz_helpers.R
@@ -0,0 +1,249 @@
+
+#Code to load packages and install them if not already installed
+if (!require("SentimentAnalysis", character.only=T, quietly=T)) {
+  install.packages("SentimentAnalysis",dependencies = T)
+  library("SentimentAnalysis", character.only=T)
+}
+
+if (!require("SentimentAnalysis", character.only=T, quietly=T)) {
+  install.packages("SentimentAnalysis",dependencies = T)
+  library("SentimentAnalysis", character.only=T)
+}
+
+if (!require("syuzhet", character.only=T, quietly=T)) {
+  install.packages("syuzhet",dependencies = T)
+  library("syuzhet", character.only=T)
+}
+
+if (!require("knitr", character.only=T, quietly=T)) {
+  install.packages("knitr",dependencies = T)
+  library("knitr", character.only=T)
+}
+
+
+#Code to load packages and install them if not already installed
+if (!require("jpeg", character.only=T, quietly=T)) {
+  install.packages("jpeg",dependencies = T)
+  library("jpeg", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("lubridate", character.only=T, quietly=T)) {
+  install.packages("lubridate",dependencies = T)
+  library("lubridate", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("shinythemes", character.only=T, quietly=T)) {
+  install.packages("shinythemes",dependencies = T)
+  library("shinythemes", character.only=T)
+}
+
+
+# #Code to load packages and install them if not already installed
+# if (!require("emo", character.only=T, quietly=T)) {
+#   install_github("hadley/emo",dependencies = T)
+#   library("", character.only=T)
+# }
+
+# devtools::install_github("hadley/emo")
+# library(emo)
+
+#Code to load packages and install them if not already installed
+if (!require("formattable", character.only=T, quietly=T)) {
+  install.packages("formattable",dependencies = T)
+  library("formattable", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("kableExtra", character.only=T, quietly=T)) {
+  install.packages("kableExtra",dependencies = T)
+  library("kableExtra", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("tm", character.only=T, quietly=T)) {
+  install.packages("tm",dependencies = T)
+  library("tm", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("SnowballC", character.only=T, quietly=T)) {
+  install.packages("SnowballC",dependencies = T)
+  library("SnowballC", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("shinydashboard", character.only=T, quietly=T)) {
+  install.packages("shinydashboard",dependencies = T)
+  library("shinydashboard", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("stringi", character.only=T, quietly=T)) {
+  install.packages("stringi",dependencies = T)
+  library("stringi", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("rcmdcheck", character.only=T, quietly=T)) {
+  install.packages("rcmdcheck",dependencies = T)
+  library("rcmdcheck", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("rmarkdown", character.only=T, quietly=T)) {
+  install.packages("rmarkdown",dependencies = T)
+  library("rmarkdown", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("googleVis", character.only=T, quietly=T)) {
+  install.packages("googleVis",dependencies = T)
+  library("googleVis", character.only=T)
+}
+
+if (!require("corrplot", character.only=T, quietly=T)) {
+  install.packages("corrplot",dependencies = T)
+  library("corrplot", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("shinyWidgets", character.only=T, quietly=T)) {
+  install.packages("shinyWidgets",dependencies = T)
+  library("shinyWidgets", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("devtools", character.only=T, quietly=T)) {
+  install.packages("devtools",dependencies = T)
+  library("devtools", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("broom", character.only=T, quietly=T)) {
+  install.packages("broom",dependencies = T)
+  library("broom", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("XML", character.only=T, quietly=T)) {
+  install.packages("XML",dependencies = T)
+  library("XML", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("stringr", character.only=T, quietly=T)) {
+  install.packages("stringr",dependencies = T)
+  library("stringr", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("rvest", character.only=T, quietly=T)) {
+  install.packages("rvest",dependencies = T)
+  library("rvest", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("shiny", character.only=T, quietly=T)) {
+  install.packages("shiny",dependencies = T)
+  library("shiny", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("reshape", character.only=T, quietly=T)) {
+  install.packages("reshape",dependencies = T)
+  library("reshape", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("tidyr", character.only=T, quietly=T)) {
+  install.packages("tidyr",dependencies = T)
+  library("tidyr", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("plotly", character.only=T, quietly=T)) {
+  install.packages("plotly",dependencies = T)
+  library("plotly", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("data.table", character.only=T, quietly=T)) {
+  install.packages("data.table",dependencies = T)
+  library("data.table", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("ggplot2", character.only=T, quietly=T)) {
+  install.packages("ggplot2",dependencies = T)
+  library("ggplot2", character.only=T)
+}
+if (!require("ggvis", character.only=T, quietly=T)) {
+  install.packages("ggvis",dependencies = T)
+  library("ggvis", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("shinycssloaders", character.only=T, quietly=T)) {
+  install.packages("shinycssloaders",dependencies = T)
+  library("shinycssloaders", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("shinyjs", character.only=T, quietly=T)) {
+  install.packages("shinyjs",dependencies = T)
+  library("shinyjs", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("DT", character.only=T, quietly=T)) {
+  install.packages("DT",dependencies = T)
+  library("DT", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("googleVis", character.only=T, quietly=T)) {
+  install.packages("googleVis",dependencies = T)
+  library("googleVis", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("knitr", character.only=T, quietly=T)) {
+  install.packages("knitr",dependencies = T)
+  library("knitr", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("dplyr", character.only=T, quietly=T)) {
+  install.packages("dplyr",dependencies = T)
+  library("dplyr", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("evaluate", character.only=T, quietly=T)) {
+  install.packages("evaluate",dependencies = T)
+  library("evaluate", character.only=T)
+}
+#Code to load packages and install them if not already installed
+if (!require("backports", character.only=T, quietly=T)) {
+  install.packages("backports",dependencies = T)
+  library("backports", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("wordcloud", character.only=T, quietly=T)) {
+  install.packages("wordcloud",dependencies = T)
+  library("wordcloud", character.only=T)
+}
+
+#Code to load packages and install them if not already installed
+if (!require("stringdist", character.only=T, quietly=T)) {
+  install.packages("stringdist",dependencies = T)
+  library("stringdist", character.only=T)
+}
+
+dir<- getwd()
+setwd(dir)
+
+#Remove all white space
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+# 
+# jscode <- "shinyjs.refresh = function() { history.go(0); }"
+
+# {"inlineContent":"Amazon calculates a product's star ratings based on a machine learned model instead of a raw data average. The model takes into account factors including the age of a rating, whether the ratings are from verified purchasers and factors that establish reviewer trustworthiness."}
+
+# # Analyze sentiment
+# sentiment <- analyzeSentiment(documents)
+# 
+# #Score table with reviews
+# Score_table <- cbind(documents, Score)
+# 
+# # View sentiment direction (i.e. positive, neutral and negative)
+# Score_table$type<- convertToDirection(sentiment$SentimentQDAP)
